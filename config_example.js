1a2151a44334a4eb7d6696d312c8b40cc08bc375
bryanvu <bryanvu@gmail.com>
2014-07-14 01:03:50 -0700

    Updated config example.

--- config_example.js
+++ config_example.js
@@ -11,14 +11,20 @@ module.exports = function() {
         shuffle_port: 80,
         insight_api_host: '127.0.0.1',
         insight_api_port: '3001',
-        network: networks.livenet      }
+        bitcoind_host: '127.0.0.1',
+        bitcoind_port: 18333,        
+        network: networks.livenet,
+        spendUnconfirmed: false      }
     default:
       return {
         shuffle_host: '127.0.0.1',
         shuffle_port: 3000,
         insight_api_host: '127.0.0.1',
         insight_api_port: '3001',
+        bitcoind_host: '127.0.0.1',
+        bitcoind_port: 18333,
         network: networks.testnet,
+        spendUnconfirmed: true
       }
   }
 }
